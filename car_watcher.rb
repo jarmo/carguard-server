d1da84a0264d2dc56899936aba9281d72722828a
Jarmo Pertman <jarmo.p@gmail.com>
2014-05-12 22:10:23 +0300

    Fix saving locations.

--- car_watcher.rb
+++ car_watcher.rb
@@ -12,14 +12,16 @@ use Rack::Auth::Basic, "Oh no, you don't!" do |username, password|
 end
 
 post "/" do
-  response = MultiJson.load(request.body.read, symbolize_keys: true).merge(
+  response = MultiJson.load(request.body.read, symbolize_keys: true)
+  response.merge!(
     latitude: response[:latitude].to_f,
     longitude: response[:longitude].to_f,
     distance: response[:distance].to_f,
     speed: response[:speed].to_f
   )
 
-  File.open("locations.txt", "w") { |f| f.puts MultiJson.dump(saved_locations << response, pretty: true) }
+  locations = saved_locations << response
+  File.open("locations.txt", "w") { |f| f.puts MultiJson.dump(locations, pretty: true) }
 
   "ok"
 end
