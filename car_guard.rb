70904bdfc95d00943add92c185bca1405e572a86
Jarmo Pertman <jarmo.p@gmail.com>
2014-05-14 00:52:57 +0300

    Update locations file name.

--- car_guard.rb
+++ car_guard.rb
@@ -7,6 +7,8 @@ require "bootstrap-sass"
 set :bind, "0.0.0.0"
 set :port, 8010
 
+LOCATIONS_FILE_NAME = "locations.json"
+
 post "/map/:api_key" do
   response = MultiJson.load(request.body.read, symbolize_keys: true)
   response.merge!(
@@ -18,7 +20,7 @@ post "/map/:api_key" do
 
   locations = saved_locations
   locations[params[:api_key].to_sym] << response
-  File.open("locations.txt", "w") { |f| f.puts MultiJson.dump(locations, pretty: true) }
+  File.open(LOCATIONS_FILE_NAME, "w") { |f| f.puts MultiJson.dump(locations, pretty: true) }
 
   "ok"
 end
@@ -33,7 +35,7 @@ get "/styles.css" do
 end
 
 def saved_locations
-  locations = File.exists?("locations.txt") ? MultiJson.load(File.read("locations.txt"), symbolize_keys: true) : {}
+  locations = File.exists?(LOCATIONS_FILE_NAME) ? MultiJson.load(File.read(LOCATIONS_FILE_NAME), symbolize_keys: true) : {}
   locations.default_proc = proc do |hash, key|
     hash[key] = []
   end
